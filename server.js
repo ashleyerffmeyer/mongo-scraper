95a14fefc2cf803ee593292959f03f3bfa2baad7
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2019-10-10 15:53:43 -0500

    updated db info

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-//mongoose.connect(MONGODB_URI);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
